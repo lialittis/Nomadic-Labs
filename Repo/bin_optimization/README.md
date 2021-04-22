7dba8b8dec37e940abd4d05cf01761f5e0c6a7fd
lialittis <yu-tianchi@outlook.com>
2021-04-22 13:51:45 +0200

    update

--- Repo/bin_optimization/README.md
+++ Repo/bin_optimization/README.md
@@ -25,40 +25,112 @@ type parsed = {
 }
 ```
 
+where *Michelson_v1_primitives.prim* is defined in */protocol_alpha/lib_protocol/michelson_v1_primitives*, and it offers method to transfer the prims to strings, which is *strings_of_prims*
+
 2. What is the labelled variable *check* ?
+
 
 3. What is *expand_all*?
 
-The labelled parameter *source* is type of String, and the second labbelled parameter *original* is the *Micheline_parser.node*, and the output is the *Micheline_parser.parsing_result*. (*To verify*)
-
+The labelled parameter *source* is type of String,
+and the second labbelled parameter *original* is the *Micheline_parser.node*,
+and the output is the *Micheline_parser.parsing_result*. (*To verify*)
 
 (* expands : expression with macros fully expanded *)
 
-4. The abstract syntax tree of Micheline expressions
+4. Micheline.node 
 
+It is the abstract syntax tree of Micheline expressions:
 The first parameter -> contain locations, can also embed custom data;
 The second parameter -> the type of preimitive names.
+
+In the michelson_v1_macros, the second paremeter is *string*.
+
 
 5. What is the difference between expanding expressions and unexpanding expressions ?
 
 expands means expression with macros fully expanded.
 
+
+6. Type of the parseing\_result ?
+
+type 'a parsing_reslut = 'a * error list
+
+In the michelson_v1_parser, the 'a type is defined as *parsed*, as in 1.
+
+
+7. canonical
+
+type 'p canonical
+
+## Translator & Interpreter
+
+### Code Review
+
+
+- Use modules : *Script_ir_translator, Script_ir_interpreter*
+- Key functions : ?
+
+This translator we use is defined in */src/protocol_alpha/lib_protocol/*,
+however it takes time to understand the methods involved.
+
+**To understand**
+
+1. *Script_repr* 
+
+defines *location*, *expr*, *node*, *error*, etc.
+
+2. *Script_int_repr*
+
+defines type num as interger and defines some methods to manipulate the num.
+
+3. Read the code in the test file of *interpretation.ml*.
+
+### Questions 
+
+
+1. Why there is *Script* module and *Script_int* module, while there is not *script.ml* or *script_int.ml* ?
+
+Answer: module could be defined in the code by keyword.
+
+2. What does this function mean?
+
+```
+let ( >>=?? ) x y =
+  x
+  >>= function
+  | Ok s ->
+      y s
+  | Error errs ->
+      Lwt.return
+      @@ Error (List.map (fun x -> Environment.Ecoproto_error x) errs)
+```
+
+Answer: Manod Programming
+
+
 ## TODO
 
 ### parsering part
 
-Expanding expressions ; 
+- [x] Expanding expressions ; 
 
+### interpreter part
+
+- [x] test the interpreter by small strings ;
+- [ ] fix the bug of reader
+- [ ] what if the type of storage or parameter ;
 
 ### dune part
 
 I. *dune-project* file
 
+- [x] construct
+- [ ] optimize
 
 II. *dune* file
 
 	1. executable file name
-
 
 	2. <package>.opam
 
@@ -76,6 +148,9 @@ II. *dune* file
 
 	Note that <package> must be non-empty, so in particular .opam files are ignored.
 
+- [x] construct
+- [ ] optimize, for example, reduce the packages
+
 III. install
 
 dune build @install
@@ -84,18 +159,7 @@ IV. build executable file
 
 dune build <name>.exe
 
-
-
-
-
-
-
-
-
-
-
-
-
+dune exec ./<name>.exe
 
 
 
---
+++ Repo/bin_optimization/main.ml
@@ -0,0 +1,253 @@
+(** Testing
+    -------
+    Component:    
+    Invocation:
+    Dependencies:
+    Subject:
+*)
+
+(*--------------- parser part ---------------------------------------*)
+
+(*print expression to string*)
+
+let print expr : string =
+  expr
+  |> Micheline_printer.printable (fun s -> s)
+  |> Format.asprintf "%a" Micheline_printer.print_expr
+
+(*-------------expanding methods--------------------*)
+
+(* expand function, with argument orginal <- ast
+ * 1. get source by print expr function
+ * 2. expand the string by expand_all function of Michelson_v1_parser module
+ * *)
+let expand original =
+  let source = print (Micheline.strip_locations original) in
+  Michelson_v1_parser.expand_all ~source ~original
+
+(*zero_loc is to define the roof of ast*)
+let zero_loc = Micheline_parser.location_zero
+
+open Michelson_v1_parser
+
+(*open Micheline*)
+open Protocol
+open Michelson_v1_primitives
+
+(*Note that, this module need to open Protocol firstly*)
+
+(* get_expanding_answer function, with argument of string (prim_name)
+ * return the expanding_answer by construct a ast prim tree -> original
+ * *)
+let expanding prim_name =
+  let ({expanded = expansion; _}, errors) =
+    expand (Prim (zero_loc, prim_name, [], []))
+  in
+  match errors with
+  | [] ->
+      let (str : string) = print (strings_of_prims expansion) in
+      print_string str
+  | _ ->
+      Format.printf "something wrong!\n"
+
+(* test by "COMPNEQ" *)
+
+;;
+expanding "CMPNEQ"
+
+(*------------parser by parser_expression-------------*)
+
+;;
+print_endline "\n"
+
+exception Get_ast
+
+let get_ast_test str =
+  let (ast, errs) = parse_expression ~check:false str in
+  ( match errs with
+  | [] ->
+      Format.printf "get_ast: success\n" ;
+      ()
+  | lst ->
+      Format.printf "get_ast: %a\n" Error_monad.pp_print_error lst ;
+      raise Get_ast ) ;
+  ast.expanded
+
+(*define the output directly*)
+(*return ast.expanded*)
+(*here, call return is to define a type converter*)
+
+(*test by a simple string*)
+let output = get_ast_test "CMPNEQ"
+
+(*print function for any expanded_result*)
+let print_expanded expanded_result =
+  let str = print (strings_of_prims expanded_result) in
+  print_string (str ^ "\n")
+
+;;
+print_expanded output
+
+(*test on file*)
+
+(*read file*)
+let read_file filename =
+  let ch = open_in filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch ; s
+
+(*
+let test_read =
+  (*let storage = "Unit" in*)
+  (*let parameter = "Int" in*)
+  let script = read_file "./contracts/hardlimit.tz" in
+  let script_ast = get_ast_test script in
+  script_ast;;
+
+test_read;;
+*)
+
+(*---------------------------interpreter part---------------------------------*)
+
+open Alpha_context
+open Script_interpreter
+
+(*Return a monad for ast.expanded*)
+let get_ast str : Script.expr tzresult Lwt.t =
+  let (ast, errs) = parse_expression ~check:false str in
+  ( match errs with
+  | [] ->
+      Format.printf "get_ast: success\n" ;
+      ()
+  | lst ->
+      Format.printf "get_ast: %a\n" Error_monad.pp_print_error lst ;
+      raise Get_ast ) ;
+  return ast.expanded
+
+(*here, call return is to define a type converter*)
+
+(*define the >>= function*)
+let ( >>=?? ) x y =
+  x
+  >>= function
+  | Ok s ->
+      y s
+  | Error err ->
+      (*Lwt.return @@ Error (List.map (fun x -> Environment.Ecoproto_error x) errs)*)
+      Lwt.return @@ Error (Environment.wrap_tztrace err)
+
+let test_context () =
+  Context.init 3
+  >>=? fun (b, _cs) ->
+  Incremental.begin_construction b
+  >>=? fun v -> return (Incremental.alpha_ctxt v)
+
+let default_source = Contract.implicit_contract Signature.Public_key_hash.zero
+
+let default_step_constants =
+  {
+    source = default_source;
+    payer = default_source;
+    self = default_source;
+    amount = Tez.zero;
+    chain_id = Chain_id.zero;
+  }
+
+module Logger : STEP_LOGGER = struct
+  let log_interp _ctxt _descr _stack = ()
+
+  let log_entry _ctxt _descr _stack = ()
+
+  let log_exit _ctxt _descr _stack = ()
+
+  let get_log () = Lwt.return (Ok None)
+end
+
+let run_step ctxt code param =
+  Script_interpreter.step
+    (module Logger)
+    ctxt
+    default_step_constants
+    code
+    param
+
+(*
+let run_script ctx ?(step_constants = default_step_constants) contract
+    ?(entrypoint = "default") ~storage ~parameter () =
+  get_ast contract
+  >>=? fun contract_expr ->
+  get_ast storage
+  >>=? fun storage_expr ->
+  get_ast parameter
+  >>=? fun parameter_expr ->
+  let script =
+    Script.{code = lazy_expr contract_expr; storage = lazy_expr storage_expr}
+  in
+  (Script_interpreter.execute
+    ctx
+    Readable
+    step_constants
+    ~script
+    ~entrypoint
+    ~parameter:parameter_expr
+    ~internal:false)
+  >>=?? fun res -> return res
+*)
+
+(*run_script function; return a monad after the interpretation*)
+let run_script ctx ?(step_constants = default_step_constants) contract
+    ?(entrypoint = "default") ~storage ~parameter () =
+  let contract_expr = Expr.from_string contract in
+  let storage_expr = Expr.from_string storage in
+  let parameter_expr = Expr.from_string parameter in
+  let script =
+    Script.{code = lazy_expr contract_expr; storage = lazy_expr storage_expr}
+  in
+  Script_interpreter.execute
+    ctx
+    Readable
+    step_constants
+    ~script
+    ~entrypoint
+    ~parameter:parameter_expr
+    ~internal:false
+  >>=?? fun res -> return res
+
+(*test the read function: bugs not fixed*)
+let test_read_file () =
+  let str = read_file "./src/bin_optimization/contracts/hardlimit.tz" in
+  print_string str
+
+;;
+test_read_file ()
+
+(*test the interpreter*)
+let test_script () =
+  test_context ()
+  >>=? fun ctx ->
+  run_script
+    ctx
+    (*(read_file "./contracts/hardlimit2.tz")*)
+    "{parameter unit; storage unit; code { CAR; NIL operation; PAIR }}"
+    ~storage:"Unit"
+    ~parameter:"Unit"
+    ()
+  >>= function
+  | Ok _ ->
+      Format.printf "test_scipt: success\n" ;
+      return_unit
+  | Error errs ->
+      Format.printf "test_scipt: fail\n" ;
+      Alcotest.failf "Unexpected error: %a" Error_monad.pp_print_error errs
+
+;;
+test_script ()
+
+(*
+
+let  =
+  let storage = "Unit" in
+  let parameter = "Int" in
+  let srcipt = read_file "./contracts/hardlimit.tz" in
+
+*)
